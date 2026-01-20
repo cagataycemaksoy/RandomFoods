b54f9093006b0dfdde5c1dc831bfb0ce7cec0e75
cagataycemaksoy <cemaksoy7890@gmail.com>
2026-01-20 13:13:36 +0100

    Initial Commit

---
+++ RandomFoods/RandomFoodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomFoodsApp.swift
+//  RandomFoods
+//
+//  Created by Cem Aksoy on 20.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct RandomFoodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
